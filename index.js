03297735b940fef2ad183e1e5b095e79c6d000ea
Heber Baraybar <heber.bar@gmail.com>
2023-10-24 09:44:28 +0200

    cambio puerto

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
 const http = require("http");
 const path = require("path");
-const {stat} = require("fs");
-const {tipo,servirFichero} = require(path.join(__dirname,"funciones.js"));
+const { stat } = require("fs");
+const { tipo, servirFichero } = require(path.join(__dirname, "funciones.js"));
 
+let puerto = process.env.PORT || 3000;
+const servidor = http.createServer((peticion, respuesta) => {
+  if (peticion.url == "/") {
+    servirFichero(respuesta, path.join(__dirname, "estaticos/index.html"), tipo("html"), 200);
+  } else {
+    let ruta = path.join(__dirname, "estaticos", peticion.url);
 
-const servidor = http.createServer((peticion,respuesta) => {
-    
-    if(peticion.url == "/"){
-        servirFichero(respuesta,path.join(__dirname,"estaticos/index.html"),tipo("html"),200);
-    }else{
-        let ruta = path.join(__dirname,"estaticos",peticion.url);
-
-        stat(ruta, (error, estadisticas) => {
-
-            if(!error && estadisticas.isFile()){
-                servirFichero(respuesta,ruta,tipo(ruta.split(".").pop()),200);
-            }else{
-                servirFichero(respuesta,path.join(__dirname,"404.html"),tipo("html"),404);
-            }
-
-        });
-
-    }
-    
+    stat(ruta, (error, estadisticas) => {
+      if (!error && estadisticas.isFile()) {
+        servirFichero(respuesta, ruta, tipo(ruta.split(".").pop()), 200);
+      } else {
+        servirFichero(respuesta, path.join(__dirname, "404.html"), tipo("html"), 404);
+      }
+    });
+  }
 });
 
-servidor.listen(3000);
-
-
+servidor.listen(puerto);
