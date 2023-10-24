5a688df3f88d4a9d1a885847690f14c66747e693
Heber Baraybar <heber.bar@gmail.com>
2023-10-24 10:27:07 +0200

    cambio

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ let puerto = process.env.PORT || 3000;
 const servidor = http.createServer((peticion, respuesta) => {
   if (peticion.url == "/") {
     servirFichero(respuesta, path.join(__dirname, "estaticos/index.html"), tipo("html"), 200);
+  } else if (peticion.url == "/color") {
+    respuesta.writeHead(200, { "Content-tye": "application/json" });
+
+    let [r, g, b] = [0, 0, 0].map(() => Math.floor(Math.random() * 256));
+    respuesta.write(JSON.stringify({ r, g, b }));
+    respuesta.end();
   } else {
     let ruta = path.join(__dirname, "estaticos", peticion.url);
 
